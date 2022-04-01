b13abe9a3dc245bef44b5cef983d7131810de063
Joji-S <georgesaracc@gmail.com>
2022-04-01 09:56:21 -0700

    c

--- MobileGame/Assets/LevelManager.cs
+++ MobileGame/Assets/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public sealed class LevelManager : MonoBehaviour {
     private static LevelManager _instance;
@@ -13,9 +14,27 @@ public sealed class LevelManager : MonoBehaviour {
         }
     }
 
-    private float _speed;
-    private float _levelLength;
-    private float _obstacleWeight;
+    [SerializeField] private LevelGenerator _levelGenerator;
+    [SerializeField] private float _speed;
+    [SerializeField] private int _levelLength;
+    [SerializeField] private float _obstacleWeight;
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.F1)) {
+            _levelGenerator.DegenerateLevel();
+        }
+        if (Input.GetKeyDown(KeyCode.F2)) {
+        }
+
+    }
+
+    private void Awake() {
+        SceneManager.sceneLoaded += OnLoad;
+    }
+    private void OnLoad(Scene scene, LoadSceneMode mode) {
+        _levelGenerator.GenerateLevel(_levelLength);
+        Debug.Log(scene.buildIndex);
+    }
 
 
 }
--- MobileGame/Assets/Scripts/LevelGenerator.cs
+++ MobileGame/Assets/Scripts/LevelGenerator.cs
@@ -6,13 +6,14 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private GameObject _startPlatform;
     [SerializeField] private GameObject _endPlatform;
     [SerializeField] List<GameObject> _platformList = new List<GameObject>();
+    Stack<GameObject> currentLevel = new Stack<GameObject>();
     private int _numberOfPlatforms;
     private int _previousPlatform;
 
     public void GenerateLevel(int levelLength) {
         _numberOfPlatforms = _platformList.Count;
         Debug.Log(_numberOfPlatforms);
-        Stack<GameObject> currentLevel = new Stack<GameObject>();
+
         currentLevel.Push(_startPlatform);
 
         Debug.Log("Generating Platforms");
@@ -24,6 +25,14 @@ public class LevelGenerator : MonoBehaviour
         Debug.Log("Adding Final Platform");
         var endPlatform = Instantiate(_endPlatform);
         endPlatform.transform.position = currentLevel.Peek().transform.GetChild(0).transform.position;
+        currentLevel.Push(endPlatform);
+    }
+
+    public void DegenerateLevel() {
+        for(int stackCount = currentLevel.Count; stackCount > 1; stackCount--) {
+            Destroy(currentLevel.Peek());
+            currentLevel.Pop();
+        }
     }
 
     private int GetPlatform() {
